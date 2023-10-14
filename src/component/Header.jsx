4207913eb1226f3235c30bfcdf6f4453d7cb8d90
Chillwithjosh <olabiyijoshua15@gmail.com>
2023-10-14 19:48:55 +0100

    design changes

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,7 @@
 export default function Header () {
     return (
         <div className="header-container">
-            <img className="header-img" src=".\public\Troll Face.png" ></img>
-            <p className="header-title">Meme Generator</p>
+            <p className="header-title"><img className="header-img" src="https://cdn-icons-png.flaticon.com/128/10851/10851206.png" ></img>Meme Generator</p>
             <p className="header-2-title">React Project</p>
         </div>
     )
